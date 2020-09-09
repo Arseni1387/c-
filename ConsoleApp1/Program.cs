ac48ee6c9e5bb20d46545a9bf5770393da73de1f
Arseni1387 <Kravzhul2002@mail.ru>
2020-09-09 14:22:43 +0300

    tgrgrv

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,8 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!eergvgfve" +
                 "tgtgtygybybhbhbhbghbyhbhbh");
+            Console.WriteLine("Hello World!eergvgfve" +
+                "tgtgtygybybhbhbhbghbyhbhbh");
         }
     }
 }
